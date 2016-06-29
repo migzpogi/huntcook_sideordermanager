36f1d4999ef6ca038b4b007473fb13350a2621ee
migze <miguel_estrella@trendmicro.com>
2016-06-29 11:35:00 +0800

    Created Orders class. Able to create orders with specified number of recipes.

--- SideOrders.py
+++ SideOrders.py
@@ -1,1 +1,29 @@
-print "Hello World"
+class Orders():
+    def get_recipes(self):
+        recipes = [
+            'Pheasant Ramen',
+            'Raven Stew',
+            'Raven Meat Pie',
+            'Rabbit Cider Soup'
+        ]
+        return recipes
+
+    def print_recipe_list(self):
+        for idx, recipe in enumerate(self.get_recipes()):
+            print idx+1, recipe
+
+    def create_order(self):
+        order = []
+        recipe_count = int(raw_input('How many recipes in this order?: '))
+        for x in range(0, recipe_count):
+            print '-'*10
+            self.print_recipe_list()
+            choice = int(raw_input('Choose recipe: '))
+            order.append(self.get_recipes()[choice-1])
+
+        return order
+
+
+if __name__ == '__main__':
+    x = Orders()
+    print x.create_order()
